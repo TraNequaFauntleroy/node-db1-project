20da07fdb9dc9b7e1003c45c3928bbb7681a7123
TraNequaFauntleroy <shecodes@nenethecreative.com>
2021-10-04 21:11:59 -0400

    checkAccountPayload MW almost done

--- api/accounts/accounts-middleware.js
+++ api/accounts/accounts-middleware.js
@@ -1,8 +1,22 @@
 const Account = require('./accounts-model')
 
 exports.checkAccountPayload = (req, res, next) => {
-  console.log('checkAccountPayload middleware')
-  next()
+  const error = { status: 400 } 
+  const {name, budget} = req.body
+  if (!name || !budget) {
+    error.message = "name and budget are required"
+    next(error)
+  } else if (typeof name !== 'string') {
+    error.message = "name of account must be a string"
+    next(error)
+  } else if (name.trim().length < 3 || name.trim().length > 100) {
+    error.message = "name of account must be between 3 and 100"
+    next(error)
+  } else if (typeof budget !== 'number' || isNaN(budget)) {
+    error.message = "budget of account must be a number"
+    next(error)
+  } 
+
 }
 
 exports.checkAccountNameUnique = (req, res, next) => {
